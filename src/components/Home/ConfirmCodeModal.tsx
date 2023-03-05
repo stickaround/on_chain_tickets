d961fd004aaa4723de729930f6d69649b96ad1bc
Benjamin Masor <benjaminmasor@gmail.com>
2023-03-05 08:52:57 -0600

    fixed

--- src/components/Home/ConfirmCodeModal.tsx
+++ src/components/Home/ConfirmCodeModal.tsx
@@ -60,7 +60,7 @@ export default function ConfirmCodeModal({
             {/*footer*/}
             <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
               <button
-                className='text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                className='text-white background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                 type='button'
                 onClick={closeModal}
               >
